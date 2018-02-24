ab72f1afd4803b1bf184ff5027814640a010284b
Andrew Walton <andrew@greenarrow.me>
2018-02-24 13:23:21 -0500

    Add google tag manager for analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import { stripIndent } from 'common-tags';
 
- // You can delete this file if you're not using it
+exports.onRenderBody = (
+  { setHeadComponents, setPreBodyComponents },
+  pluginOptions
+) => {
+  if (process.env.NODE_ENV === `production`) {
+    setHeadComponents([
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-98114991-2"
+      />,
+      <script
+        key="plugin-google-tagmanager"
+        dangerouslySetInnerHTML={{
+          __html: stripIndent`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-98114991-2');`,
+        }}
+      />,
+    ]);
+
+    setPreBodyComponents([
+      <noscript
+        key="plugin-google-tagmanager"
+        dangerouslySetInnerHTML={{
+          __html: stripIndent`
+            <iframe
+              src="https://www.googletagmanager.com/gtag/js?id=UA-98114991-2"
+              height="0"
+              width="0"
+              style="display: none; visibility: hidden"
+            ></iframe>`,
+        }}
+      />,
+    ]);
+  }
+};
